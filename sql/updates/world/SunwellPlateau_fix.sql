ceb2a838b4ad51e8c83b312336295786fea2b3db
Vitasic Vitasic <vitasic-pokataev@yandex.ru>
2012-04-21 02:10:50 +0400

    SunwellPlateau_fix

---
+++ sql/updates/world/SunwellPlateau_fix.sql
@@ -0,0 +1,19 @@
+DELETE FROM  creature_template_addon WHERE entry = 24895;
+UPDATE creature SET position_x = 1466.53 WHERE id = 24895;
+UPDATE creature SET position_y = 644.949 WHERE id = 24895;
+UPDATE creature SET position_z = 20.2059 WHERE id = 24895;
+INSERT INTO waypoint_data VALUES ('30000', '1', '1495.77', '573.492', '23.9099', '0', '1', '0', '100', '0');
+INSERT INTO waypoint_data VALUES ('30000', '2', '1476.75', '611.606', '23.695', '0', '1', '0', '100', '0');
+INSERT INTO waypoint_data VALUES ('31000', '3', '1466.53', '644.949', '20.2059', '0', '1', '0', '100', '0');
+INSERT INTO waypoint_data VALUES ('31000', '2', '1466.64', '644.949', '54.398', '7000', '1', '0', '100', '0');
+INSERT INTO waypoint_data VALUES ('32000', '1', '1458.59', '629.443', '20.573', '0', '1', '0', '100', '0');
+INSERT INTO waypoint_data VALUES ('31000', '1', '1465.81', '678.581', '54.398', '0', '1', '0', '100', '0');
+INSERT INTO waypoint_data VALUES ('34000', '1', '1475.33', '672.628', '64.5133', '1', '0', '0', '100', '0');
+INSERT INTO waypoint_data VALUES ('33000', '1', '1480.59', '606.804', '20.018', '0', '1', '0', '100', '0');
+
+INSERT INTO waypoint_data VALUES ('34000', '1', '1475.33', '672.628', '64.5133', '1', '1', '0', '100', '0');
+INSERT INTO waypoint_data VALUES ('34000', '2', '1456.34', '695.644', '66.7257', '1', '1', '0', '100', '0');
+INSERT INTO waypoint_data VALUES ('34000', '3', '1425.8', '601.801', '68.7822', '1', '1', '0', '100', '0');
+INSERT INTO waypoint_data VALUES ('34000', '4', '1502.26', '533.484', '70.3252', '1', '1', '0', '100', '0');
+INSERT INTO waypoint_data VALUES ('34000', '5', '1531.75', '545.337', '64.7583', '1', '1', '0', '100', '0');
+INSERT INTO waypoint_data VALUES ('34000', '6', '1457.36', '604.974', '20.7713', '1', '1', '0', '100', '0');
